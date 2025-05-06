f6c6f0f9ff91860981b65582ddd05ed23bf54d32
ZetaCygnus01 <122745586+ZetaCygnus01@users.noreply.github.com>
2025-05-06 21:02:31 +0000

    CreandoMenu

---
+++ api-harrypotter/src/components/TabNavigation.jsx
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router-dom";
+import { FaHatWizard, FaBook, FaHome, FaFilm, FaStar, FaInfo } from "react-icons/fa";
+
+const tabs = [
+    { path: '/', icon: <FaHatWizzard/>, label: 'Personajes'},
+    { path: '/houses', icon: <FaHome/>, label: 'Casas'},
+    { path: '/movies', icon: <FaFilm/>, label: 'Peliculas'},
+    { path: '/books', icon: <FaBook/>, label: 'Libros'},
+    { path: '/favorites', icon: <FaStar/>, label: 'Favoritos'},
+    { path: '/info', icon: <FaInfo/>, label: 'Info'},
+];
+
+const TabNabigation 0 () => {
+    return (
+        <nav className="fixed bottom-0 left-0 right-0 bg-purple-900 text-white flex justify-around py-3 shadow-md z-50">
+          {tabs.map(tab => (
+            <NavLink
+              key={tab.path}
+              to={tab.path}
+              className={({ isActive }) =>
+                `flex flex-col items-center text-sm ${isActive ? 'text-yellow-300' : 'text-white'}`
+              }
+            >
+              {tab.icon}
+              <span>{tab.label}</span>
+            </NavLink>
+          ))}
+        </nav>
+      );
+    };
+
+export default TabNabigation;
